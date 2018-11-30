399985f060fddc7d162cd8bdda8693fb1d8a5671
jspiegel <jspiegel@pitt.edu>
2018-11-30 16:35:59 -0500

    edit sub script

--- mpi_gypsum.sh
+++ mpi_gypsum.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=gypsum_mpi
 #SBATCH --output=crc_mpi_gypsum_out.txt
 #SBATCH --time=0:07:00
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=2
+#SBATCH --nodes=12
+#SBATCH --ntasks-per-node=12
 #SBATCH --cluster=mpi
 #SBATCH --partition=opa
 
@@ -13,5 +13,5 @@ module load intel/2017.1.132 intel-mpi/2017.1.132
 module load python/anaconda2.7-4.2.0
 
 ## Run the process
-srun -n $SLURM_NTASKS --mpi=pmi2 python run_gypsum.py -j mpi_sample_molecules.json > test_mpi_gypsum_output.txt
+srun -n $SLURM_NTASKS --mpi=pmi2 python run_gypsum.py -q -j mpi_sample_molecules.json > test_mpi_gypsum_output.txt
 
