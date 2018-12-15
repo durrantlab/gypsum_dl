71bf19211358a35abd3732746cfcba88d38c5833
Jacob Durrant <jdurrant@MacBook-Pro.fios-router.home>
2018-12-15 13:43:09 -0500

    Docstrings and comments for SaveToSDF.py.

--- gypsum/Steps/IO/SaveToSDF.py
+++ gypsum/Steps/IO/SaveToSDF.py
@@ -1,3 +1,5 @@
+"""Saves output files to SDF."""
+
 import __future__
 
 import gypsum.Utils as Utils
@@ -9,16 +11,26 @@ except:
     raise ImportError("You need to install rdkit and its dependencies.")
 
 def save_to_sdf(contnrs, params, separate_output_files, output_file):
-    """
-    Saves the 3D models to the disk as an SDF file.
+    """Saves the 3D models to the disk as an SDF file.
+
+    :param contnrs: A list of containers (MolContainer.MolContainer).
+    :type contnrs: list
+    :param params: The parameters.
+    :type params: dict
+    :param separate_output_files: Whether save each molecule to a different
+       file.
+    :type separate_output_files: bool
+    :param output_file: The output file (or partial file name if saving to
+       multiple files).
+    :type output_file: str
     """
 
+    # Save an empty molecule with the parameters.
     if separate_output_files == False:
         w = Chem.SDWriter(output_file)
     else:
         w = Chem.SDWriter(output_file + ".params.sdf")
 
-    # Save an empty molecule too with the parameters.
     m = Chem.Mol()
     m.SetProp("_Name", "EMPTY MOLECULE DESCRIBING GYPSUM PARAMETERS")
     for param in params:
@@ -29,11 +41,17 @@ def save_to_sdf(contnrs, params, separate_output_files, output_file):
         w.flush()
         w.close()
 
+    # Also save the file or files containing the output molecules.
     Utils.log("Saving molecules associated with...")
     for i, contnr in enumerate(contnrs):
+        # Add the container properties to the rdkit_mol object so they get
+        # written to the SDF file.
         contnr.add_container_properties()
+
+        # Let the user know which molecule you're on.
         Utils.log("\t" + contnr.orig_smi)
 
+        # Save the file(s).
         if separate_output_files == True:
             w = Chem.SDWriter(output_file + "." + str(i + 1) + ".sdf")
 
