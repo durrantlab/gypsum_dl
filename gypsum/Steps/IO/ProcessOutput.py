c523632b1b4ca1f8353632c8d11388c377588d4b
Jacob Durrant <jdurrant@MacBook-Pro.fios-router.home>
2018-12-15 13:24:22 -0500

    Added docstrings and comments to ProcessOutput.py and SaveToPDB.py.

--- gypsum/Steps/IO/ProcessOutput.py
+++ gypsum/Steps/IO/ProcessOutput.py
@@ -1,3 +1,6 @@
+"""The proccess_output definition determines which formats are saved to the
+disk (output)."""
+
 import __future__
 
 from gypsum.Steps.IO.SaveToSDF import save_to_sdf
@@ -5,19 +8,22 @@ from gypsum.Steps.IO.SaveToPDB import convert_sdfs_to_PDBs
 from gypsum.Steps.IO.Web2DOutput import web_2d_output
 
 def proccess_output(contnrs, params):
-    """
-    Proccess outputing molecular models.
-    """
+    """Proccess the molecular models in preparation for writing them to the
+       disk."""
+
+    # Unpack some variables.
     separate_output_files = params["separate_output_files"]
     output_file = params["output_file"]
     output_folder = params["output_folder"]
 
     if output_file.lower().endswith(".html"):
+        # Write to an HTML file.
         web_2d_output(contnrs, output_file)
     else:
+        # Write to an SDF file.
         save_to_sdf(contnrs, params, separate_output_files, output_file)
 
-
+    # Also write to PDB files, if requested.
     if params["output_pdb"] == True:
         print("\nMaking PDB output files\n")
         convert_sdfs_to_PDBs(contnrs, output_folder)
--- gypsum/Steps/IO/SaveToPDB.py
+++ gypsum/Steps/IO/SaveToPDB.py
@@ -1,3 +1,5 @@
+"""Contains the function for saving the output to PDB files."""
+
 import __future__
 
 import glob
@@ -11,35 +13,38 @@ import rdkit.Chem as Chem
 rdkit.RDLogger.DisableLog('rdApp.*')
 
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),'gypsum'))
-# from gypsum.gypsum import MolObjectHandling as MOH
-
 
 def convert_sdfs_to_PDBs(contnrs, output_folder):
-    """
-    This will convert every conformer into a PDB file saved in the output_folder.
-    These .pdb files will be named after using the ID provided by the source file (or a modified name in the case of untitled or duplicate names)
-        plus "__{}".format(i) where i is the conformer number
+    """This will convert every conformer into a PDB file, which is saved in
+       the output_folder. The .pdb files are named using the ID in the source
+       file (or a modified name in the case of untitled or duplicate names)
+       plus "__{}".format(i), where i is the conformer number
 
-    Input:
-    :param str gen_folder_path: Path of the folder for the current generation
-    :param str SDFs_folder_path: Path of the folder with all of the 3D .sdf files to convert
+    :param contnrs: A list of containers (MolContainer.MolContainer).
+    :type contnrs: list
+    :param output_folder: The name of the output folder.
+    :type output_folder: str
     """
-    for ligand in contnrs:
-        ligand.add_container_properties()
 
-        name = ligand.name
-        mols = ligand.mols
+    # Go through each container.
+    for contnr in contnrs:
+        contnr.add_container_properties()
+
+        # Get the molecule name and associated variants.
+        name = contnr.name
+        mols = contnr.mols
 
         conformer_counter = 0
+        # Got through the variants.
         for m in mols:
             pdb_file = "{}{}__{}.pdb".format(output_folder, name, conformer_counter)
+
+            # Get the conformers into the rdkit_mol object.
             m.load_conformers_into_rdkit_mol()
             mol = m.rdkit_mol
             if mol == None:
                 continue
             else:
-                # Write to a PDB file
-                Chem.MolToPDBFile(mol,pdb_file,flavor=4)
+                # Write conformers to a PDB file.
+                Chem.MolToPDBFile(mol, pdb_file, flavor = 4)
             conformer_counter += 1
-
-#
