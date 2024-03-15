8e0d3c0595e9e329f1b60dc8fd9efd668535dbc1
Alex Morehead <alex.morehead@gmail.com>
2024-03-15 12:44:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     description="Gypsum-DL is a free, open-source program that converts 1D and 2D small-molecule representations (SMILES strings or flat SDF files) into 3D models. It outputs models with alternate ionization, tautomeric, chiral, cis/trans isomeric, and ring-conformational states.",
     author="Durrant Lab",
     url="https://github.com/durrantlab/gypsum_dl",
-    install_requires=["rdkit", "numpy", "scipy", "mpi4py", "setuptools"],
+    install_requires=[
+        "rdkit",
+        "numpy",
+        "scipy",
+        "mpi4py",
+        "setuptools",
+        "git+https://github.com/amorehead/dimorphite_dl.git@patch-1"
+    ],
     packages=find_packages(),
 )
